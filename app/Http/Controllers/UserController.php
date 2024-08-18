3ca90c6b8acff248effc7ea77adfafb8708ac0ae
Luan Silva <88367567+Luan16p@users.noreply.github.com>
2024-08-18 18:51:45 -0400

    Desativando Regra de CORS

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    public function validateCredentials(Request $request) {
-        
+    public function submit_credentials(Request $request) {
+        return $request->json()->all();
+    }
+
+    public function access_credentials(Request $request) {
+        return User::find($request->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +17,6 @@ use App\Http\Controllers\AuthController;
 Route::get('/', function () {
     return 'Hello World!';
 });
+
+Route::get('/access-credentials/{id}', [UserController::class, 'access_credentials'])->name('access-credentials');
+Route::post('/submit-credentials', [UserController::class, 'submit_credentials'])->name('submit-credentials');
